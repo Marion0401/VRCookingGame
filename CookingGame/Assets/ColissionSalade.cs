bdd862ad61c8ee8cdcc926f74aeddb64853232ca
PaulWau <paul.waultre@isen.yncrea.fr>
2022-02-01 15:05:48 +0100

    Couper salade
    Méthode pour faire 4 feuilles de salade quand on tape 3 fois dedans

--- CookingGame/Assets/ColissionSalade.cs
+++ CookingGame/Assets/ColissionSalade.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class ColissionSalade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Collider MaindCollider;
+    public int nbcoup = 3;
+    public GameObject FeuilleSalade;
+    private void OnTriggerEnter(Collider other)
     {
-        
-}
+        Debug.Log("test");
+        if (other == MaindCollider)
+        {
+            Debug.Log("enter");
+            nbcoup--;
+        }
+    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (nbcoup == 0)
+        {
+            Debug.Log("Salade");
+
+            for (int i = -1; i <= 2; i++)
+            {
+                Instantiate(FeuilleSalade, transform.position+new Vector3(0,1.5f*i,0), 
+                    transform.rotation*Quaternion.Euler(new Vector3(0,3*i,0)));
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
