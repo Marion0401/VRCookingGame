8ea5335febc71b46ccdeea1b2b933312c6866929
TeoGras <teo.gras@student.junia.fr>
2022-02-02 16:51:54 +0100

    Ajout assemblage des hamburger
    Pour que sa marche, les ingrédients doivent etre dans la layer ingrédient, l'assiette doit avec un enfant qui sera la zone de collision autorisé pour mettre l'ingredient sur l'assiette. Cette même zone de collision doit avoir le scip assemblageIngrédients en composant.
    Il manque le désassemblage et la vérification des recettes

---
+++ CookingGame/Assets/Scripts/AssemblageIngrédients.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblageIngrédients : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject _Assiette;
+    [SerializeField] private GameObject _IngredientHolder;
+    private List<GameObject> _ListIngredients = new List<GameObject>();
+    private float totalLocalHauteurY;
+    private float ancienneHauteurY;
+    private LayerMask _ingredientLayer;
+    private LayerMask _assietteLayer;
+    private void Start()
+    {
+        totalLocalHauteurY = 1f;//1 car l'assiette sera le parent de tout or les enfant se réfère au scale du parent
+        _ingredientLayer = LayerMask.NameToLayer("Ingredients");
+        _assietteLayer = LayerMask.NameToLayer("Assiettes");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Quand on détecte qu'un objet rentre dans dans la zone de collision 
+        //On vérifie qu'il n'appartient pas a IngredientHolder ( donc qu'il n'ai plus attraper par le joueur )
+        if (other.gameObject.transform.parent.name == _IngredientHolder.transform.name)
+        {
+            //On vérifie que c'est un ingrédient
+            if (other.gameObject.layer == _ingredientLayer)
+            {
+                //On définie sa nouvelle position
+                NewAssemblerMakerPosition(other.gameObject.transform.localScale.y);
+                //On le met en enfant de l'assiette
+                Join(other.gameObject,_Assiette);
+                //On ajoute le gameObject a la list
+                _ListIngredients.Add(other.gameObject);
+            }
+        }
+            
+    }
+
+    private void Update()
+    {
+        Debug.Log(_ListIngredients.Count);
+    }
+
+
+    private void Join(GameObject child, GameObject parent)
+    {
+        child.layer = _assietteLayer;//L'ingrédient devient une partie de l'assiette
+        child.transform.parent = parent.transform;
+        totalLocalHauteurY += child.transform.localScale.y/2f + ancienneHauteurY;
+        ancienneHauteurY = child.transform.localScale.y / 2f;
+        child.transform.localPosition = new Vector3(0f,totalLocalHauteurY,0f);
+        child.transform.localRotation = Quaternion.identity;
+        child.GetComponent<Rigidbody>().isKinematic = true;
+
+    }
+
+    private void NewAssemblerMakerPosition(float yMovement)
+    {
+        transform.position += new Vector3(0,yMovement, 0);
+    }
+}
