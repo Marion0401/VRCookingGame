9fdbaf382645d8a04056a2382ee2614a53652d3a
Marion0401 <71750635+Marion0401@users.noreply.github.com>
2022-02-21 09:19:25 +0100

    commit

--- CookingGame/Assets/Scripts/RecipesManager.cs
+++ CookingGame/Assets/Scripts/RecipesManager.cs
@@ -5,19 +5,36 @@ using UnityEngine;
 public class RecipesManager : MonoBehaviour
 {
     public GameObject dialogueBubble;
-    public List<Recipes> listRecipes = new List<Recipes>();
+    public List<Recipes> listHamburger = new List<Recipes>();
+    public List<GameObject> listDrink = new List<GameObject>();
+    public GameObject fries;
+    //public float xPositionTopBread;
+    //public float yPositionTopBread;
+    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        int rand=Random.Range(0, listRecipes.Count);
-        Recipes actualRecipe = listRecipes[rand];
-        foreach(GameObject ingredient in actualRecipe.ingredientRecipes)
-        {
-            GameObject burger = Instantiate(ingredient, new Vector3(-0.558f, 0.127f, 0), Quaternion.identity);
-            burger.transform.SetParent(dialogueBubble.transform, false);
-        }
         
+        // pour le burger
+        int rand = Random.Range(0, listHamburger.Count);
+        Recipes actualRecipe = listHamburger[rand];
+        foreach (GameObject ingredient in actualRecipe.ingredientRecipes)
+        {
+            GameObject burger = Instantiate(ingredient, new Vector3(0,0, 0), Quaternion.identity);
+            
+            burger.transform.SetParent(dialogueBubble.transform, false);
+            
+        }
+
+        // pour les frites
+
+        // pour la boisson
+
+
+
     }
 
     // Update is called once per frame
