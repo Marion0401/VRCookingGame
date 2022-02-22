d9483c5b411181567c67d1806c4b213b9917bdf9
PaulWau <paul.waultre@isen.yncrea.fr>
2022-02-22 14:50:12 +0100

    fin aliment decoupe

--- CookingGame/Assets/Collision.cs
+++ CookingGame/Assets/Collision.cs
@@ -3,16 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 namespace DefaultNamespace
 {
-    public class Collision
+    public class Collision :MonoBehaviour
     {
-        {
-        private GameObject alimentdebase;
-        private GameObject decoupe;
-        
-        public ParticleSystem particulecoup;
+
+        public float nbalimentdecoupe;
+        public GameObject decoupe;
         public Collider MaindCollider;
         public int nbcoup = 3;
         private void OnTriggerEnter(Collider other)
@@ -32,13 +31,13 @@ namespace DefaultNamespace
             {
                 Debug.Log("Salade");
             
-                for (int i = -1; i <= 2; i++)
+                for (int i = -1; i <=nbalimentdecoupe-2; i++)
                 {
                     Instantiate(decoupe, transform.position+new Vector3(0,1.5f*i,0), 
                         transform.rotation*Quaternion.Euler(new Vector3(0,3*i,0)));
                 }
             
-                Destroy(alimentdebase);
+                Destroy(this.gameObject);
             }
         }
     }
---
+++ CookingGame/Assets/collisions.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public enum Aliments
+    {
+        SALADE,
+        TOMATE,
+        PAIN
+    }
+
+    public class collisions : MonoBehaviour
+    {
+        private GameObject alimentdebase;
+        private GameObject decoupe;
+        public Aliments aliment;
+        public ParticleSystem particulecoup;
+        public Collider MaindCollider;
+        public int nbcoup = 3;
+        
+
+        private void Start()
+        {
+            switch (aliment)
+            {
+                case (Aliments.TOMATE):
+                {
+                    alimentdebase = Instantiate(Resources.Load("Tomato", typeof(GameObject))) as GameObject;
+                    //alimentdebase= Resources.Load<GameObject>("=Tomato");
+                    decoupe = Resources.Load<GameObject>("Tranche_Tomate");
+                    Rigidbody rb = alimentdebase.AddComponent<Rigidbody>() as Rigidbody;
+                    var aaa = alimentdebase.GetComponent<Collision>();
+                    aaa.MaindCollider = MaindCollider;
+                    aaa.decoupe = decoupe;
+                    aaa.nbalimentdecoupe = 4;
+                    rb.useGravity = false;
+                    break;
+                }
+                case (Aliments.SALADE):
+                {
+                    alimentdebase = Instantiate(Resources.Load("Sla", typeof(GameObject))) as GameObject;
+                    //alimentdebase= Resources.Load<GameObject>("=Sla");
+                    decoupe = Resources.Load<GameObject>("FeuilleSalade");
+                    Rigidbody rb = alimentdebase.AddComponent<Rigidbody>() as Rigidbody;
+                    var aaa = alimentdebase.GetComponent<Collision>();
+                    aaa.MaindCollider = MaindCollider;
+                    aaa.decoupe = decoupe;
+                    aaa.nbalimentdecoupe = 4;
+                    rb.useGravity = false;
+                    break;
+                }
+                case (Aliments.PAIN):
+                {
+
+                    alimentdebase = Instantiate(Resources.Load("Bread", typeof(GameObject))) as GameObject;
+                    //alimentdebase= Resources.Load<GameObject>("=Sla");
+                    decoupe = Resources.Load<GameObject>("Tranche_Pain");
+                    Rigidbody rb = alimentdebase.AddComponent<Rigidbody>() as Rigidbody;
+                    var aaa = alimentdebase.GetComponent<Collision>();
+                    aaa.MaindCollider = MaindCollider;
+                    aaa.decoupe = decoupe;
+                    aaa.nbalimentdecoupe = 2;
+                    rb.useGravity = false;
+                    break;
+                }
+            }
+            //Instantiate(alimentdebase);
+
+
+        }
+
+    }
+}
