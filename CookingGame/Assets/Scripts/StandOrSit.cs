8f2eaa2aabd29ff47aaa40730b1dd83854cf8928
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2022-03-01 13:13:29 +0100

    taille

--- CookingGame/Assets/Scripts/StandOrSit.cs
+++ CookingGame/Assets/Scripts/StandOrSit.cs
@@ -9,11 +9,11 @@ public class StandOrSit : MonoBehaviour
         string player = PlayerPrefs.GetString("position");
         if (player == "Sitting")
         {
-            gameObject.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            gameObject.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
         }
         else
         {
-            gameObject.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            gameObject.transform.localScale = new Vector3(0.9f, 0.7f, 0.9f);
         }
     }
 
